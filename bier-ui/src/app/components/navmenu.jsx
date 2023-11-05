f77ba8c8e965ab75cf71055cec8835653180fc50
imat19 <vergara.reymart82@gmail.com>
2023-11-06 00:42:09 +0800

    added

--- bier-ui/src/app/components/navmenu.jsx
+++ bier-ui/src/app/components/navmenu.jsx
@@ -1,11 +1,9 @@
-import Link from "next/link"
-import Image from "next/image"
-import Logo from './img/Sandra-logo.png'
-import { usePathname } from "next/navigation"
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "./img/Sandra-logo.png";
+import { usePathname } from "next/navigation";
 
 export default function NavMenu() {
-
-
   return (
     <nav className="navbar navbar-expand-lg py-0 navbar-color shadow">
       <div className="container">
@@ -17,18 +15,47 @@ export default function NavMenu() {
             width={100}
           />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <Link className="nav-link navmenu border-start py-3" aria-current="page" href="/">Home</Link>
-            <Link className="nav-link navmenu border-start py-3" href="/menu">Menu</Link>
-            <Link className="nav-link navmenu border-start py-3" href="/contact">Contact</Link>
-            <Link className="nav-link navmenu border-start py-3 border-end" href="/about">About</Link>
+            <Link
+              className="nav-link navmenu border-start py-3"
+              aria-current="page"
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className="nav-link navmenu border-start py-3"
+              href="/services"
+            >
+              Services
+            </Link>
+            <Link
+              className="nav-link navmenu border-start py-3"
+              href="/contact"
+            >
+              Contact
+            </Link>
+            <Link
+              className="nav-link navmenu border-start py-3 border-end"
+              href="/about"
+            >
+              About
+            </Link>
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
